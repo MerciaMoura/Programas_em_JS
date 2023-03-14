73a955705e2c37123bbbfcb8dd76fb6d2ab7db55
MerciaMoura <merciamoura.pe@hotmail.com>
2023-03-14 14:45:01 -0300

    atv02 commit

---
+++ Q1/index.js
@@ -0,0 +1,13 @@
+
+let nota;
+
+do {
+  nota = prompt("Informe uma nota de 0 a 10:");
+
+  if (nota < 0 || nota > 10) {
+    alert("Valor inválido! Digite uma nota entre 0 e 10.");
+  }
+} while (nota < 0 || nota > 10);
+
+alert("Nota válida: " + nota);
+
---
+++ Q2/index.js
@@ -0,0 +1,20 @@
+function Login() {
+
+    const usuario = usuario=document.getElementById("usuario").value;
+    usuario=usuario.toLowerCase();
+    const senha=document.getElementById("senha").value;
+    senha=senha.toLowerCase();
+
+    if(usuario == "" || senha == "") { 
+             alert("Os campos usuário e senha são obrigatório"); }
+
+             else (usuario=="admin" && senha=="admin"){
+                 alert("Login realizado com sucesso!")
+
+             }
+
+
+             
+    }
+    
+  
